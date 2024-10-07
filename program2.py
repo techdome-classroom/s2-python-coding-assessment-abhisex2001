9abc97745cc2f10540a5300c5c84ebb9d9676fe9
abhisex2001 <abossabhishek@gmail.com>
2024-10-07 14:18:17 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,10 +1,33 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+def romanToInt(s: str) -> int:
+   
+    roman_map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    total = 0
+    length = len(s)
+    
+    for i in range(length):
+        value = roman_map[s[i]]
+        
+        if i + 1 < length and roman_map[s[i + 1]] > value:
+            total -= value 
+        else:
+            total += value  
+    
+    return total
+
+# Example usage:
+print(romanToInt("III"))
+print(romanToInt("LVIII"))    
+print(romanToInt("MCMXCIV"))  
+
 
 
 
